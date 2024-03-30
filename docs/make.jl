98c4be2de6115984f9f33511a10382a19bb97877
drizk1 <rizkytennis@gmail.com>
2024-03-30 11:32:16 -0400

    adds documentation files

---
+++ docs/make.jl
@@ -0,0 +1,34 @@
+using Documenter, DocumenterMarkdown
+using Tidier, TidierDB
+
+DocTestMeta = quote
+    using Tidier, TidierDB, DataFrames, Chain
+end
+DocMeta.setdocmeta!(TidierDB,
+    :DocTestSetup,
+    DocTestMeta;
+    recursive=true)
+makedocs(
+    modules=[TidierDB],
+    clean=true,
+    doctest=false,
+    #format   = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    sitename="TidierDB.jl",
+    authors="Karandeep Singh et al.",
+    strict=[
+        :doctest,
+        :linkcheck,
+        :parse_error,
+        :example_block,
+        # Other available options are
+        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block,
+        # :footnote, :meta_block, :missing_docs, :setup_block
+    ], checkdocs=:all, format=Markdown(), draft=false,
+    build=joinpath(@__DIR__, "docs")
+)
+
+deploydocs(; repo="github.com/TidierOrg/TidierDB.jl.git", push_preview=true,
+    deps=Deps.pip("mkdocs", "pygments", "python-markdown-math", "mkdocs-material",
+        "pymdown-extensions", "mkdocstrings", "mknotebooks",
+        "pytkdocs_tweaks", "mkdocs_include_exclude_files", "jinja2"),
+    make=() -> run(`mkdocs build`), target="site", devbranch="main")
---
+++ docs/src/reference.md
@@ -0,0 +1,22 @@
+```@meta
+DocTestSetup= quote
+using TidierDB
+end
+```
+## Index
+```@index
+Modules = [TidierDB]
+Private = false
+```
+
+## Reference - Exported functions
+```@autodocs
+Modules = [TidierDB]
+Private = false
+```
+
+## Reference - Internal functions
+```@autodocs
+Modules = [TidierDB]
+Public = false
+```

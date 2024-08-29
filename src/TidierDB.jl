c76cca1a5d9ef7369c018fa3c1c7a90cf9808b1f
Daniel Rizk <rizkytennis@gmail.com>
2024-08-29 12:08:13 -0400

    fix type casting as part of larger mutation

--- src/TidierDB.jl
+++ src/TidierDB.jl
@@ -150,9 +150,13 @@ function finalize_query(sqlquery::SQLQuery)
     if !isempty(sqlquery.ch_settings) && current_sql_mode[] == clickhouse()
         complete_query = complete_query * " \n " * string(sqlquery.ch_settings)
     end
+    
     complete_query = replace(complete_query, "&&" => " AND ", "||" => " OR ",
      "FROM )" => ")" ,  "SELECT SELECT " => "SELECT ", "SELECT  SELECT " => "SELECT ", "DISTINCT SELECT " => "DISTINCT ", 
-     "SELECT SELECT SELECT " => "SELECT ", "PARTITION BY GROUP BY" => "PARTITION BY", "GROUP BY GROUP BY" => "GROUP BY", "HAVING HAVING" => "HAVING", )
+     "SELECT SELECT SELECT " => "SELECT ", "PARTITION BY GROUP BY" => "PARTITION BY", "GROUP BY GROUP BY" => "GROUP BY", "HAVING HAVING" => "HAVING", 
+     r"var\"(.*?)\"" => s"\1")
+      
+     complete_query = replace(complete_query, ", AS " => " AS ")
 
     if current_sql_mode[] == postgres() || current_sql_mode[] == duckdb() || current_sql_mode[] == mysql() || current_sql_mode[] == mssql() || current_sql_mode[] == clickhouse() || current_sql_mode[] == athena() || current_sql_mode[] == gbq() || current_sql_mode[] == oracle()  || current_sql_mode[] == snowflake() || current_sql_mode[] == databricks()
         complete_query = replace(complete_query, "\"" => "'", "==" => "=")
